8cc5a0cacd95c7b2e1223aec502b7f1ccab2fc53
b-612 <fromtheorion@yandex.ru>
2020-04-09 01:28:17 +0300

    Добавляет бронь полям ввода чисел, на самые разные случаи пользовательского ввода

--- src/js/modules/_sell-apartment.js
+++ src/js/modules/_sell-apartment.js
@@ -1,20 +1,34 @@
 import { numberWithSpaces } from '../functions';
 
 $(()=>{
-  const $numberItems = $('.sell-form__input[name=price], .sell-form__input[name=square]');
+  const $numberInputs = $('.sell-form__input[name=price], .sell-form__input[name=square]');
 
-  if (!$numberItems) {
+  if (!$numberInputs) {
     return false;
   }
 
-  $numberItems.on('input', function () {
+  const BACKSPACE__KEYCODE = 8;
+  let prevValue = '';
+
+  $numberInputs.on('input', function () {
     const lastChar = this.value[this.value.length - 1];
 
-    this.value = this.value.replace(/[A-Za-zА-Яа-яЁё]/, '');
-    this.value = this.value.replace (/\D/, '');
+    this.value = this.value
+      .replace(/[A-Za-zА-Яа-яЁё]/, '')
+      .replace (/\D/, '');
 
     if (this.value !== '' && $.isNumeric(lastChar)) {
+      prevValue = Number(this.value.replace(/\s/g, ''));
       this.value = numberWithSpaces(Number(this.value.replace(/\s/g, '')));
+    } else {
+      this.value = numberWithSpaces(prevValue);
+    }
+  });
+
+  $numberInputs.on('keydown', function (evt) {
+    if (evt.keyCode === BACKSPACE__KEYCODE && this.value.length === 1) {
+      prevValue = '';
+      this.value = prevValue;
     }
   });
 });
