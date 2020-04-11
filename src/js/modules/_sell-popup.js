3bdf20351502b1456ce6af9fb9ada66e2629ac47
b-612 <fromtheorion@yandex.ru>
2020-04-11 15:03:54 +0300

    Недвижка. Переделывает реализацию попапов так, что первый у нас кастомный, а второй magnific popup

--- src/js/modules/_sell-popup.js
+++ src/js/modules/_sell-popup.js
@@ -1,40 +1,96 @@
 $(() => {
+  const ESC_KEYCODE = 27;
+  const ANIMATION_OPEN_TIME = 500;
   const ANIMATION_CLOSE_TIME = 300;
 
   const $body = $('body');
+  const $overlay = $('.sell-apartment__overlay');
   const $sellFormBtn = $('.sell-form__btn');
   const $contactBuyerBtn = $('.sell-popup__btn');
   const $firstPopup = $('.sell-popup');
-  const $closeFirstBtn = $('.sell-popup__close');
   const $secondPopup = $('.sell-order-popup');
   const $closeSecondBtn = $('.sell-order-popup__close');
+  let isFirstOpened = false;
 
-  const animateOpen = (popup) => {
-    popup.addClass('sell-popup__open-animation');
-    popup.removeClass('sell-popup__close-animation');
+  const removeCloseFirstListeners = () => {
+    const $closeBtn = $('.sell-popup__close');
+
+    $closeBtn.off('click', closeFirstPopup);
+    $overlay.off('click', closeFirstPopup);
+    $(window).off('keydown');
   };
 
-  const animateClose = (popup) => {
-    popup.addClass('sell-popup__close-animation');
-    popup.removeClass('sell-popup__open-animation');
+  const onFirstPopupClose = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closeFirstPopup();
+    }
   };
 
-  $sellFormBtn.magnificPopup({
-    type: 'inline',
-    preloader: false,
-    removalDelay: ANIMATION_CLOSE_TIME,
-    callbacks: {
-      open: () => {
-        animateOpen($firstPopup);
-      },
-      beforeClose: () => {
-        animateClose($firstPopup)
-      }
-    },
-  });
+  const setCloseFirstListeners = () => {
+    const $closeBtn = $('.sell-popup__close');
 
-  $closeFirstBtn.click(function() {
-    $.magnificPopup.close();
+    $closeBtn.click(closeFirstPopup);
+    $overlay.click(closeFirstPopup);
+    $(window).on('keydown', onFirstPopupClose.bind());
+  };
+
+  const openSecondPop = (popup) => {
+    popup.addClass('sell-order-popup__open-animation');
+    popup.removeClass('sell-order-popup__close-animation');
+    removeCloseFirstListeners();
+  };
+
+  const closeSecondPop = (popup) => {
+    popup.addClass('sell-order-popup__close-animation');
+    popup.removeClass('sell-order-popup__open-animation');
+    setCloseFirstListeners();
+  };
+
+  const openFirstPopup = () => {
+    isFirstOpened = true;
+    const bodyOldWidth = $body.width();
+
+    $firstPopup.css('display', 'block')
+      .animate({
+        right: 0,
+        opacity: 1
+      }, ANIMATION_OPEN_TIME, () => {
+        if (isFirstOpened) {
+          setCloseFirstListeners();
+        }
+      })
+      .attr('tabindex', 0)
+      .focus();
+
+    $overlay.fadeIn(ANIMATION_OPEN_TIME).css('z-index', '601');
+    $body.css({
+      position: 'absolute',
+      width: bodyOldWidth,
+      left: 0,
+      top: 0,
+      overflowY: 'hidden'
+    });
+  };
+
+  const closeFirstPopup = () => {
+    $firstPopup.animate({
+      right: '-100%',
+      opacity: 0
+    }, ANIMATION_CLOSE_TIME, function () {
+      $(this).css({
+        display: 'none'
+      });
+    }).removeAttr('tabindex');
+
+    $overlay.fadeOut(ANIMATION_CLOSE_TIME);
+    $body.removeAttr('style');
+
+    removeCloseFirstListeners();
+    isFirstOpened = false;
+  };
+
+  $sellFormBtn.click(() => {
+    openFirstPopup();
   });
 
   $contactBuyerBtn.magnificPopup({
@@ -43,15 +99,15 @@ $(() => {
     removalDelay: ANIMATION_CLOSE_TIME,
     callbacks: {
       open: () => {
-        animateOpen($secondPopup);
+        openSecondPop($secondPopup);
       },
       beforeClose: () => {
-        animateClose($secondPopup)
+        closeSecondPop($secondPopup)
       }
     },
   });
 
-  $closeSecondBtn.click(function() {
+  $closeSecondBtn.click(function () {
     $.magnificPopup.close();
   });
 });
