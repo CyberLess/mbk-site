3b8960b68b4411c18e32cc9572f325d4f0403971
b-612 <fromtheorion@yandex.ru>
2020-05-12 02:10:10 +0300

    Дорабатывает вращение барабана и появление попапа после выпадения выигрышного баннера

--- src/js/modules/_form.js
+++ src/js/modules/_form.js
@@ -67,6 +67,10 @@ $(()=>{
 				},
 				square: {
 					required: true
+				},
+				auctionEmail: {
+					required: true,
+					email: true
 				}
 			}
 		})
--- src/js/modules/success-auction.js
+++ src/js/modules/success-auction.js
@@ -2,10 +2,12 @@ import { getRandomInt } from "../functions";
 
 $(() => {
   const ANIMATION_TIMEOUT = 7000;
+  const ITEMS_HALF_MARGIN = 4;
+  const POPUP_OPEN_TIMEOUT = 500;
 
   const $list = $('.success-auction__carusel');
   const $items = $list.find('.success-auction__item');
-  const $testButton = $('.success-auction__prompt');
+  const $form = $('.success-auction__form');
 
   const x = getRandomInt(30, 60);
 
@@ -15,21 +17,53 @@ $(() => {
     }
   };
 
+  const openPopupWinner = ($item) => {
+    const $itemCopy = $item.clone();
+
+    $itemCopy.addClass('success-auction__item_opened');
+
+    $.magnificPopup.open({
+      items: {
+        src: $itemCopy,
+        type: 'inline'
+      },
+      afterClose: () => {
+        $itemCopy.remove();
+      }
+    });
+  };
+
+  const getWinnerItem = () => {
+    const $indicator = $('.success-auction__indicator');
+    const indicatorCoords = $indicator.offset().left;
+    const $items = $('.success-auction__item');
+
+    $items.css('opacity', '0.2');
+    $items.each(function () {
+      if (parseInt($(this).offset().left) - ITEMS_HALF_MARGIN <= indicatorCoords && parseInt($(this).offset().left + $(this).outerWidth()) + ITEMS_HALF_MARGIN >= indicatorCoords + $indicator.outerWidth()) {
+        $(this).css('opacity', '1');
+        setTimeout(() => {
+          openPopupWinner($(this));
+        }, POPUP_OPEN_TIMEOUT);
+        return 'winner';
+      }
+    });
+  };
+
   const rotateWheel = () => {
     $list.animate({
       right: ((x * $items.outerWidth())+(x * ($items.length - getRandomInt(1, 6))))
-    }, ANIMATION_TIMEOUT, 'easeInOutCubic');
+    }, ANIMATION_TIMEOUT, 'easeInOutCubic', getWinnerItem);
   };
 
   pushItemsInList(x);
 
   let counter = 0;
 
-  $list.on('mousedown', function (evt) {
-    if (counter < 1) {
+  $form.on('submit', () => {
+    if (counter < 1 && !$form.children().hasClass('error')) {
       rotateWheel();
+      counter += 1;
     }
-
-    counter += 1;
   });
 });
