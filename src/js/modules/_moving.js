924dcdeca604d74a3940bfef9882751309c50fb1
CyberLess <47046834+CyberLess@users.noreply.github.com>
2020-05-11 21:32:16 +0300

    кредиты

--- src/js/modules/_moving.js
+++ src/js/modules/_moving.js
@@ -1,27 +1,38 @@
 (() => {
-	if(!$('.js-move-item').length)
-		return false;	
+	if (!$(".js-move-item").length) return false;
 
-	$(window).on('load resize', () => {
-
+	$(window).on("load resize", () => {
 		let ww = $(window).width();
 
-		$('.js-move-item').each((i, el) => {
-
+		$(".js-move-item").each((i, el) => {
 			let $this = $(el);
-			
-			let $to = $($this.data('to'));
-			let $from = $($this.data('from'));
 
-			let point = $this.attr('data-point') ? parseInt($this.data('point')) : 1024;
+			let $to = $($this.data("to"));
+			let $from = $($this.data("from"));
 
-			if(ww <= point)
-				$this.appendTo($to).addClass('is-moved')
-			else
-				if($this.hasClass('is-moved'))
-					$this.appendTo($from).removeClass('is-moved')
+			let method = $this.attr("data-method")
+				? $(this).data("method")
+				: "append";
 
-		})		
-	})
+			let point = $this.attr("data-point")
+				? parseInt($this.data("point"))
+				: 1024;
 
-})($)
+			if (ww <= point) {
+				if (method == "append") {
+					$this.appendTo($to).addClass("is-moved");
+				} else {
+					$this.prependTo($to).addClass("is-moved");
+				}
+			} else {
+				if ($this.hasClass("is-moved")) {
+					if (method == "append") {
+						$this.appendTo($from).removeClass("is-moved");
+					} else {
+						$this.prependTo($from).removeClass("is-moved");
+					}
+				}
+			}
+		});
+	});
+})($);
