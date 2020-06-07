65677271ac4be9a850e0f340646b0c79b4954135
CyberLess <47046834+CyberLess@users.noreply.github.com>
2020-06-07 23:39:39 +0300

    правки статьи, новостей и прочих

--- src/js/modules/_article-title.js
+++ src/js/modules/_article-title.js
@@ -1,46 +1,46 @@
 (() => {
-  function isScrolledIntoView(el) {
-    const rect = el.getBoundingClientRect();
-    const elemTop = rect.top;
-    const elemBottom = rect.bottom;
-    const isVisible = elemTop < window.innerHeight && elemBottom >= el.clientHeight / 2;
-    return isVisible;
-  }
+	function isScrolledIntoView(el) {
+		const rect = el.getBoundingClientRect();
+		const elemTop = rect.top;
+		const elemBottom = rect.bottom;
+		const isVisible =
+			elemTop < window.innerHeight && elemBottom >= el.clientHeight / 2;
+		return isVisible;
+	}
 
-  const cont = document.querySelector(`.js-article`);
-  const title = document.querySelector(`.js-title`);
-  const link = document.querySelector(`.js-link`);
+	const cont = document.querySelector(`.js-article`);
+	const title = document.querySelector(`.js-title`);
+	const link = document.querySelector(`.js-link`);
 
-  window.addEventListener(`scroll`, () => {
-    if (title && link) {
-      if (isScrolledIntoView(title)) {
-        link.classList.add(`is-active`);
-      } else {
-        link.classList.remove(`is-active`);
-      }
-    }
-  });
+	// window.addEventListener(`scroll`, () => {
+	//   if (title && link) {
+	//     if (isScrolledIntoView(title)) {
+	//       link.classList.add(`is-active`);
+	//     } else {
+	//       link.classList.remove(`is-active`);
+	//     }
+	//   }
+	// });
 
-  let winWidth = $(window).width();
+	let winWidth = $(window).width();
 
-  if (winWidth < 581) {
-    const $toggle = $('.js-toggle-title');
-    window.addEventListener(`scroll`, () => {
-      if (title && cont) {
-        if (isScrolledIntoView(title)) {
-          $toggle.addClass(`active`);
-        } else if (!isScrolledIntoView(cont)) {
-          $toggle.removeClass(`active`);
-          $('.article-item__title-wrapper').removeClass('opened');
-        }
-      }
-    });
-    $toggle.on('click',  e => {
-      $('.article-item__title-wrapper').addClass('opened');
-    });
-    $('.js-title-close').on('click',  e => {
-      $('.article-item__title-wrapper').removeClass('opened');
-    });
-  }
-
+	if (winWidth < 581) {
+		const $toggle = $(".js-toggle-title");
+		window.addEventListener(`scroll`, () => {
+			if (title && cont) {
+				if (isScrolledIntoView(title)) {
+					$toggle.addClass(`active`);
+				} else if (!isScrolledIntoView(cont)) {
+					$toggle.removeClass(`active`);
+					$(".article-item__title-wrapper").removeClass("opened");
+				}
+			}
+		});
+		$toggle.on("click", (e) => {
+			$(".article-item__title-wrapper").addClass("opened");
+		});
+		$(".js-title-close").on("click", (e) => {
+			$(".article-item__title-wrapper").removeClass("opened");
+		});
+	}
 })($);
