9327795cc6d29466d7a7bef7462edd707bb0f286
b-612 <fromtheorion@yandex.ru>
2020-06-09 01:30:36 +0300

    Правка слайдера

--- src/js/modules/_ba.js
+++ src/js/modules/_ba.js
@@ -4,67 +4,85 @@ $(function() {
 	if(!$('.ba').length)
 		return false;
 
+	const MOBILE_MAX_WIDTH = 768;
+	let isMobile = $(window).width() <= MOBILE_MAX_WIDTH;
+
 	var relXPer = 0;
 	var relYPer = 0;
 	var slider, handle, arrow;
 	var requestID;
 	var degree;
 
-	$('.ba').on('mousemove touchmove touchstart', function(e) {
+	const onMousemoveTouchmoveTouchstart = function(evt) {
+		evt.preventDefault();
 
-		e.preventDefault();
+		const moveSlider = function (thisEl) {
+			var touchstart = evt.type === 'touchstart' || evt.type === 'touchmove',
+				e = touchstart ? evt.originalEvent : evt,
+				pageX = touchstart ? e.targetTouches[0].pageX : e.pageX,
+				pageY = touchstart ? e.targetTouches[0].pageY : e.pageY;
 
-		var touchstart = e.type === 'touchstart' || e.type === 'touchmove',
-			e = touchstart ? e.originalEvent : e,
-			pageX = touchstart ? e.targetTouches[0].pageX : e.pageX,
-			pageY = touchstart ? e.targetTouches[0].pageY : e.pageY;
+			slider = thisEl;
+			handle = slider.find('.ba__handle');
+			arrow = slider.find('.ba__arrow');
 
-		slider = $(this);
-		handle = slider.find('.ba__handle');
-		arrow = slider.find('.ba__arrow');
+			var offset = slider.offset();
 
-		var offset = slider.offset();
+			var width = slider.outerWidth()
+			var height = slider.outerHeight()
 
-		var width = slider.outerWidth()
-		var height = slider.outerHeight()
+			var relX = pageX - offset.left;
+			var relY = pageY - offset.top;
 
-		var relX = pageX - offset.left;
-		var relY = pageY - offset.top;
+			relXPer = 100 / (width / relX);
+			relYPer = 100 / (height / relY);
 
-		relXPer = 100 / (width / relX);
-		relYPer = 100 / (height / relY);
+			var center_x = (offset.left) + (width/2);
+			var center_y = (offset.top) + (height/2);
+			var mouse_x = pageX; var mouse_y = pageY;
+			var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+			degree = (radians * (180 / Math.PI) * -1) + 90;
 
-		var center_x = (offset.left) + (width/2);
-		var center_y = (offset.top) + (height/2);
-		var mouse_x = pageX; var mouse_y = pageY;
-		var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
-		degree = (radians * (180 / Math.PI) * -1) + 90;
+			requestID = window.requestAnimationFrame(ba_update);
+		};
 
-		requestID = window.requestAnimationFrame(ba_update);
+		if (!isMobile && $('.ba__inner:active').length > 0) {
+			moveSlider($(this));
+		}
 
-	}).on('mouseleave', function() {
+		if (isMobile) {
+			moveSlider($(this));
+		}
+	};
 
-		handle = $(this).find('.ba__handle');
-		arrow = $(this).find('.ba__arrow');
+	const onMouseleave = function() {
+		if ($('.we-sell__comment-opener:hover').length === 0 && $('.we-sell__help:hover').length === 0) {
+			handle = $(this).find('.ba__handle');
+			arrow = $(this).find('.ba__arrow');
 
-		arrow.css({
-			'transform': 'rotate(180deg)'
-		})
+			arrow.css({
+				'transform': 'rotate(180deg)'
+			})
 
-		handle.css({
-			'left': '50%'
-		})
+			handle.css({
+				'left': '50%'
+			})
 
-		$(this).addClass('is-mouseleave').find('.ba__resize').css({
-			'width': '50%'
-		}).on(transitionEnd, function(e) {
-			$(this).off(transitionEnd).removeAttr('style').closest('.ba').addClass('js-animation-play')
-		})
+			$(this).addClass('is-mouseleave').find('.ba__resize').css({
+				'width': '50%'
+			}).on(transitionEnd, function(e) {
+				$(this).off(transitionEnd).removeAttr('style').closest('.ba').addClass('js-animation-play')
+			})
 
-		window.cancelAnimationFrame(requestID);
-	}).on('mouseenter', function() {
+			window.cancelAnimationFrame(requestID);
+		}
+	};
+
+	const onMouseEnter = function() {
 		$(this).removeClass('is-mouseleave js-animation-play').find('.ba__resize').off(transitionEnd)
-	})
+	};
+
+	$('.ba').on('mousemove touchmove touchstart', onMousemoveTouchmoveTouchstart).on('mouseleave', onMouseleave).on('mouseenter', onMouseEnter);
 
 	function ba_update() {
 		if(slider.hasClass('is-mouseleave'))
@@ -82,15 +100,20 @@ $(function() {
 			'transform': 'rotate('+degree+'deg)'
 		})
 	}
-})
 
-$(window).on('load resize', function() {
-	if(!$('.ba').length)
-		return false;
+	$(window).on('load resize', function() {
+		if(!$('.ba').length)
+			return false;
 
-	$('.ba__image').each(function() {
-		var width = $(this).closest('.ba').outerWidth()
-		// console.log("width", width)
-		$(this).css('width', width+"px")
-	})
-})
+		$('.ba__image').each(function() {
+			var width = $(this).closest('.ba').outerWidth()
+			// console.log("width", width)
+			$(this).css('width', width+"px")
+		})
+	});
+
+	$(window).resize(() => {
+		$('.ba').off();
+		$('.ba').on('mousemove touchmove touchstart', onMousemoveTouchmoveTouchstart).on('mouseleave', onMouseleave).on('mouseenter', onMouseEnter);
+	});
+});
